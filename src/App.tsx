b62829cc1d072f361dd94c3403a5d4ed24b1706c
efoneli <abbaefoneli@gmail.com>
2023-05-15 15:36:51 +0100

    finishing

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ const App = () => {
     <>
     <GlobalStyle />
     <Wrapper >
-    <h1>REACT QUIZ</h1>
+    <h1>QUIZ BOX</h1>
     {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
     <button className='start' onClick={startTrivia}>
       Start
